a2df53eabdba7863ba555a3cbc00510e439e4972
Zaidan Yahya <zaidanyahya28@gmail.com>
2020-10-05 13:01:09 +0900

    add blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,6 +31,11 @@ def create_app(test_config=None):
     # Authentication
     from . import auth
     app.register_blueprint(auth.bp)
+    
+    # Blog (Index)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     # a simple page that says hello
     @app.route('/')
